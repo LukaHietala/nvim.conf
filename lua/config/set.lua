b83e227163ae02dab556aaf3f20a17691e604dbd
Luka Hietala <luka.hietala08@gmail.com>
2023-01-25 15:46:34 +0200

    fixed copilot

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -22,3 +22,8 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
+
+-- Copilot needs this...
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
